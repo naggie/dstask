75bb2d1a9cb2c84f082d38399514ce5262a27d55
Callan Bryant <callan.bryant@gmail.com>
2018-12-08 20:34:50 +0000

    start filter methof

--- dstask/task.go
+++ dstask/task.go
@@ -35,3 +35,18 @@ type Task struct {
 	resolved time.Time
 	due time.Time
 }
+
+type TaskSet struct {
+	tasks []Task
+}
+
+type TaskFilter struct {
+	text string
+	tags []string
+	project string
+	priority int
+}
+
+func (t *TaskSet) filter(filter *TaskFilter) TaskSet {
+
+}
