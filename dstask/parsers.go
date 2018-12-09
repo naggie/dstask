c297ea210935e72ac52e46df112795c073a1f532
Callan Bryant <callan.bryant@gmail.com>
2018-12-09 20:54:51 +0000

    start taskwarrior parser

--- dstask/parsers.go
+++ dstask/parsers.go
@@ -1,5 +1,10 @@
 package dstask
 
+import (
+	"encoding/json"
+	"os"
+)
+
 func parseTaskLine(args []string) {
 
 }
@@ -16,6 +21,23 @@ func parseFile(filepath string) {
 
 }
 
+type TaskWarriorTask struct {
+	Description string `json:"description"`
+	End string `json:"end"`
+	Entry string `json: entry`
+	Id int `json: id`
+	Modified string `json: modified`
+	Status string `json: status`
+	Tags []string `json: tags`
+	Uuid string `json: uuid`
+}
+
 func (ts *TaskSet) ImportFromTaskwarrior() {
+	var taskWarriorTasks []TaskWarriorTask
 	// from stdin
+	err := json.NewDecoder(os.Stdin).Decode(&taskWarriorTasks)
+
+	if (err != nil) {
+		return err
+	}
 }
