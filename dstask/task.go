c518ddb858f02e0fbf19e8eb732289bdef174db2
Callan Bryant <callan.bryant@gmail.com>
2018-12-14 19:41:14 +0000

    move parser into task.go

--- dstask/task.go
+++ dstask/task.go
@@ -5,6 +5,8 @@ package dstask
 import (
 	"sort"
 	"time"
+	"strconv"
+	"strings"
 )
 
 const (
@@ -147,6 +149,9 @@ func (ts *TaskSet) MaybeAddTask(task Task) bool {
 // when refering to tasks by ID, NORMAL_STATUSES must be loaded exclusively --
 // even if the filter is set to show issues that have only some statuses.
 type TaskLine struct {
+	// operation, taken from first occurance. Mapped to a method to operate on
+	// tasks.
+	Action string
 	Id       int
 	Tags     []string
 	AntiTags []string
@@ -162,3 +167,37 @@ type TaskLine struct {
 //func (t *Task) Save() error {
 //
 //}
+
+func ParseTaskLine(args []string) *TaskLine {
+	var id int
+	var tags []string
+	var antiTags []string
+	var project string
+	var priority string
+	var words []string
+
+	for i, item := range args {
+		if s, err := strconv.ParseInt(item, 10, 64); i == 0 && err == nil {
+			id = int(s)
+		} else if strings.HasPrefix(item, "project:") {
+			project = item[8:len(item)]
+		} else if len(item) > 2 && item[0:1] == "+" {
+			tags = append(tags, item[1:len(item)])
+		} else if len(item) > 2 && item[0:1] == "-" {
+			antiTags = append(antiTags, item[1:len(item)])
+		} else if IsValidPriority(item) {
+			priority = item
+		} else {
+			words = append(words, item)
+		}
+	}
+
+	return &TaskLine{
+		Id:       id,
+		Tags:     tags,
+		AntiTags: antiTags,
+		Project:  project,
+		Priority: priority,
+		Text:     strings.Join(words, " "),
+	}
+}
