fef0e09d8fae0b799b7499f4f7657ae769ed88bf
Callan Bryant <callan.bryant@gmail.com>
2018-12-22 11:28:21 +0000

    update help to reflect intentions

--- help.go
+++ help.go
@@ -15,25 +15,19 @@ Available commands:
 
 add          : Add a task
 start        : Change task status to active
+annotate     : Append or edit notes for a task
 stop         : Change task status to pending
 resolve      : Resolve a task
-comment      : Add a comment to a task
 context      : Set global context for task list and new tasks
 modify       : set attributes for a task
 edit         : Edit task with text editor
-describe     : Add a detailed description in text editor
 undo         : Undo last action with git revert
 git          : Pass a command to git in the repository. Used for push/pull.
-
 day          : Show tasks completed since midnight in current context
 week         : Show tasks completed within the last week
 projects     : List projects with completion status
-
-import-tw    : Import tasks from taskwarrior
-
+import-tw    : Import tasks from taskwarrior via stdin
 help         : Get help on any command or show this message
-
-
 `)
 	os.Exit(1)
 }
