fdcb1385b21f0669e3e353520a814a05cee39b27
Callan Bryant <callan.bryant@gmail.com>
2019-03-15 17:38:33 +0000

    add fake git creds to keep git happy

--- smoketest.sh
+++ smoketest.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 # Just a quick smoke test to check nothing major is broken as a start of CI
 
+# TODO use ! to invert exit status
+# TODO comments guiding the process
+
 set -x
 set -e
 
@@ -8,6 +11,9 @@ go build cmd/dstask.go
 
 mkdir ~/.dstask
 git -C ~/.dstask init
+
+git -C ~/.dstask config user.email "you@example.com"
+git -C ~/.dstask config user.name "Test user"
 
 ./dstask add test task +foo project:bar
 ./dstask start 1
