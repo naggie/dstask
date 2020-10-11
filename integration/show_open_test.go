70be65557f893e07bfbd270cea9095c44a7c8602
Coleman McFarland <coleman.mcfarland+dontlaugh@gmail.com>
2020-10-11 12:55:51 -0400

    add basic show-open tests

--- integration/show_open_test.go
+++ integration/show_open_test.go
@@ -1,3 +1,48 @@
 package integration
 
-// TODO: what is a good integration test for "show-open"?
+import (
+	"testing"
+
+	"github.com/naggie/dstask"
+	"gotest.tools/assert"
+)
+
+func TestShowOpen(t *testing.T) {
+	repo, cleanup := makeDstaskRepo(t)
+	defer cleanup()
+
+	program := testCmd(repo)
+
+	output, exiterr, success := program("add", "+one", "one")
+	assertProgramResult(t, output, exiterr, success)
+
+	output, exiterr, success = program("add", "+two", "two")
+	assertProgramResult(t, output, exiterr, success)
+
+	output, exiterr, success = program("show-open")
+	assertProgramResult(t, output, exiterr, success)
+
+	var tasks []dstask.Task
+
+	tasks = unmarshalTaskArray(t, output)
+	assert.Equal(t, tasks[0].Summary, "one", "one should be sorted first")
+
+	output, exiterr, success = program("context", "-one")
+	assertProgramResult(t, output, exiterr, success)
+
+	output, exiterr, success = program("show-open")
+	assertProgramResult(t, output, exiterr, success)
+
+	tasks = unmarshalTaskArray(t, output)
+	assert.Equal(t, tasks[0].Summary, "two", "setting -one as a context")
+
+	output, exiterr, success = program("2", "done")
+	assertProgramResult(t, output, exiterr, success)
+
+	output, exiterr, success = program("show-open")
+	assertProgramResult(t, output, exiterr, success)
+
+	tasks = unmarshalTaskArray(t, output)
+	assert.Equal(t, len(tasks), 0, "no tasks open in this context")
+
+}
