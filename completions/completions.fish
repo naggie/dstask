2ac9bcf1b153f1e0ce5787f0ac2fe409032a5d5c
AvianAnalyst <Kylan@byrd.digital>
2026-06-30 13:24:12 -0700

    Update fish completion to not run a command
    Also removes completion commands from the auto completion

--- completions/completions.fish
+++ completions/completions.fish
@@ -1,6 +1,4 @@
 #!/usr/bin/env fish
 
-complete -f -c dstask -a (echo (dstask _completions) | string collect)
-#complete -f -c task -a (echo (task _completions) | string collect)
-#complete -f -c t -a (echo (t _completions) | string collect)
+complete -f -k -c dstask -a "next add rm remove template log start note notes stop done resolve context modify edit undo sync open git show-next show-projects show-tags show-active show-paused show-open show-resolved show-templates show-unorganised help version P0 P1 P2 P3"
 
